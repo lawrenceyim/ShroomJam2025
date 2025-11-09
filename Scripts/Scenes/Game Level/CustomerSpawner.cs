4983bb646431ca63b1d87f5026a34467dc0e4020
lawrenceyim <lawrence.yim.dev@gmail.com>
2025-11-09 14:18:08 -0500

    Added counter placeholder.

--- Scripts/Scenes/Game Level/CustomerSpawner.cs
+++ Scripts/Scenes/Game Level/CustomerSpawner.cs
@@ -1,6 +1,14 @@
 using Godot;
 
 public partial class CustomerSpawner : Node, ITick {
+    [Export]
+    private int _customerSpawndelayMax;
+
+    [Export]
+    private int _customerSpawndelayMin;
+
+    private int _customerSpawndelay;
+
     public void PhysicsTick(double delta) {
         throw new System.NotImplementedException();
     }
--- Scripts/Scenes/Game Level/ShelfView.cs
+++ Scripts/Scenes/Game Level/ShelfView.cs
@@ -2,55 +2,55 @@ using System.Collections.Generic;
 using Godot;
 
 public partial class ShelfView : Node2D {
-    [Export]
-    private PackedScene _dvdSlotPackedScene;
+	[Export]
+	private PackedScene _dvdSlotPackedScene;
 
-    // private readonly Vector2I _originPosition = new Vector2I(330, 270);
-    private readonly Vector2I _originPosition = new Vector2I(0, 0);
+	// private readonly Vector2I _originPosition = new Vector2I(330, 270);
+	private readonly Vector2I _originPosition = new Vector2I(0, 0);
 
-    private readonly int _xOffset = 170;
-    private readonly int _yOffset = 200;
-    private DvdService _dvdService;
-    private PackedSceneRepository _packedSceneRepository;
-    private Vector2I? _hoveredSlot;
-    private Dictionary<Vector2I, DvdSlot> _dvdSlots = new();
-    private Texture2dRepository _texture2dRepository;
+	private readonly int _xOffset = 170;
+	private readonly int _yOffset = 200;
+	private DvdService _dvdService;
+	private PackedSceneRepository _packedSceneRepository;
+	private Vector2I? _hoveredSlot;
+	private Dictionary<Vector2I, DvdSlot> _dvdSlots = new();
+	private Texture2dRepository _texture2dRepository;
 
-    public void Initialize(DvdService dvdService, Texture2dRepository texture2dRepository) {
-        _dvdService = dvdService;
-        _texture2dRepository = texture2dRepository;
-        Vector2I shelfSize = dvdService.GetShelfSize();
-        GD.Print(shelfSize);
+	public void Initialize(DvdService dvdService, Texture2dRepository texture2dRepository) {
+		_dvdService = dvdService;
+		_texture2dRepository = texture2dRepository;
+		Vector2I shelfSize = dvdService.GetShelfSize();
+		GD.Print(shelfSize);
 
-        for (int column = 0; column < shelfSize.X; column++) {
-            for (int row = 0; row < shelfSize.Y; row++) {
-                DvdSlot dvdSlot = _dvdSlotPackedScene.Instantiate() as DvdSlot;
-                Vector2I position = new Vector2I(column, row);
-                dvdSlot.Initialize(position);
-                dvdSlot.Position = _originPosition + new Vector2I(column * _xOffset, row * _yOffset);
-                dvdSlot.Initialize(position);
-                dvdSlot.MouseEntered += (Vector2I position) => {
-                    GD.Print(position);
-                    _hoveredSlot = position;
-                };
-                dvdSlot.MouseExited += () => _hoveredSlot = null;
-                _dvdSlots[position] = dvdSlot;
-                AddChild(dvdSlot);
+		for (int column = 0; column < shelfSize.X; column++) {
+			for (int row = 0; row < shelfSize.Y; row++) {
+				DvdSlot dvdSlot = _dvdSlotPackedScene.Instantiate() as DvdSlot;
+				Vector2I position = new Vector2I(column, row);
+				dvdSlot.Initialize(position);
+				dvdSlot.Position = _originPosition + new Vector2I(column * _xOffset, row * _yOffset);
+				dvdSlot.Initialize(position);
+				dvdSlot.MouseEntered += (Vector2I position) => {
+					GD.Print(position);
+					_hoveredSlot = position;
+				};
+				dvdSlot.MouseExited += () => _hoveredSlot = null;
+				_dvdSlots[position] = dvdSlot;
+				AddChild(dvdSlot);
 
-                Dvd dvd = _dvdService.GetDvdFromShelf(new Vector2I(column, row));
-                if (dvd != null) {
-                    // set sprite based on tier and color of the dvd                    
-                    SetDvdTexture(position, _texture2dRepository.GetTexture(Texture2dId.Placeholder));
-                }
-            }
-        }
-    }
+				Dvd dvd = _dvdService.GetDvdFromShelf(new Vector2I(column, row));
+				if (dvd != null) {
+					// set sprite based on tier and color of the dvd                    
+					SetDvdTexture(position, _texture2dRepository.GetTexture(Texture2dId.Placeholder));
+				}
+			}
+		}
+	}
 
-    public void SetDvdTexture(Vector2I slot, Texture2D texture) {
-        _dvdSlots[slot].SetTexture(texture);
-    }
+	public void SetDvdTexture(Vector2I slot, Texture2D texture) {
+		_dvdSlots[slot].SetTexture(texture);
+	}
 
-    public Vector2I? GetHoveredSlot() {
-        return _hoveredSlot;
-    }
+	public Vector2I? GetHoveredSlot() {
+		return _hoveredSlot;
+	}
 }
