dd084834b3befca6fa282898a90032497c77c234
lawrenceyim <lawrence.yim.dev@gmail.com>
2025-11-13 08:21:32 -0500

    Added shelf background and started working on combing upgrade and end of day screen.

--- Scripts/Scenes/End of Day/EndOfDayScreen.cs
+++ Scripts/Scenes/End of Day/EndOfDayScreen.cs
@@ -10,6 +10,32 @@ public partial class EndOfDayScreen : Node2D {
     [Export]
     private Label _profitLabel;
 
+    [Export]
+    private Button _customerRarityUpgradeButton;
+
+    [Export]
+    private Button _merchandiseRarityUpgradeButton;
+
+    [Export]
+    private Button _nextDayButton;
+
+    [Export]
+    private Label _customerRarityLevelLabel;
+
+    [Export]
+    private Label _merchandiseRarityLevelLabel;
+
+    [Export]
+    private Label _customerRarityUpgradeCostLabel;
+
+    [Export]
+    private Label _merchandiseRarityUpgradeCostLabel;
+
+    [Export]
+    private Label _moneyLabel;
+
+    private UpgradeService _upgradeService;
+    private PlayerDataService _playerDataService;
     private int _moneyEarned;
     private int _moneyCounter;
 
@@ -19,6 +45,14 @@ public partial class EndOfDayScreen : Node2D {
         _moneyEarned = transactionService.GetProfitFromDay();
         transactionService.AddProfitFromDayToPlayerMoney();
 
+        _playerDataService = serviceLocator.GetService<PlayerDataService>(ServiceName.PlayerData);
+        _upgradeService = serviceLocator.GetService<UpgradeService>(ServiceName.Upgrade);
+
+        _customerRarityUpgradeButton.Pressed += () => { UpgradeRarity(UpgradeService.UpgradeType.CustomerRarity); };
+        _merchandiseRarityUpgradeButton.Pressed += () => { UpgradeRarity(UpgradeService.UpgradeType.MerchandiseRarity); };
+        _moneyLabel.Text = $"{_playerDataService.GetMoney()}";
+        SetUpgradeCost(UpgradeService.UpgradeType.CustomerRarity);
+        SetUpgradeCost(UpgradeService.UpgradeType.MerchandiseRarity);
 
         if (!GlobalSettings.MuteVolume) {
             _soundEffectPlayer.Play();
@@ -32,5 +66,25 @@ public partial class EndOfDayScreen : Node2D {
 
         _moneyCounter = Math.Min(_moneyCounter + 25, _moneyEarned);
         _profitLabel.Text = $"Profit: {_moneyCounter}";
+    }
+
+
+    public void UpgradeRarity(UpgradeService.UpgradeType upgradeType) {
+        _upgradeService.UpgradeRarity(upgradeType);
+        SetUpgradeCost(upgradeType);
+        _moneyLabel.Text = $"{_playerDataService.GetMoney()}";
+    }
+
+    public void SetUpgradeCost(UpgradeService.UpgradeType upgradeType) {
+        switch (upgradeType) {
+            case UpgradeService.UpgradeType.CustomerRarity:
+                _customerRarityUpgradeCostLabel.Text = $"{_upgradeService.ComputeUpgradeCost(upgradeType)}";
+                _customerRarityLevelLabel.Text = $"Customer Rarity Level: {_playerDataService.GetCustomerRarityUpgradeLevel()}";
+                break;
+            case UpgradeService.UpgradeType.MerchandiseRarity:
+                _merchandiseRarityUpgradeCostLabel.Text = $"{_upgradeService.ComputeUpgradeCost(upgradeType)}";
+                _merchandiseRarityLevelLabel.Text = $"Merchandise Rarity Level: {_playerDataService.GetMerchandiseRarityUpgradeLevel()}";
+                break;
+        }
     }
 }
--- Scripts/Service Locator/ServiceImpl/TransactionService.cs
+++ Scripts/Service Locator/ServiceImpl/TransactionService.cs
@@ -5,67 +5,67 @@ using ServiceSystem;
 namespace InputSystem;
 
 public class TransactionService : IService {
-    private MerchandiseService _merchandiseService;
-    private PlayerDataService _playerDataService;
-    private const int ValuePerTier = 100;
-    private int _profitFromDay = 0;
+	private MerchandiseService _merchandiseService;
+	private PlayerDataService _playerDataService;
+	private const int ValuePerTier = 100;
+	private int _profitFromDay = 0;
 
-    public void Initialize(MerchandiseService merchandiseService, PlayerDataService playerDataService) {
-        _merchandiseService = merchandiseService;
-        _playerDataService = playerDataService;
-    }
+	public void Initialize(MerchandiseService merchandiseService, PlayerDataService playerDataService) {
+		_merchandiseService = merchandiseService;
+		_playerDataService = playerDataService;
+	}
 
-    public int SellMerchandise(CustomerSaleDto saleDto) {
-        Merchandise merchandise = _merchandiseService.GetHeldMerchandise();
-        _merchandiseService.SetHeldMerchandise(null);
-        int profit = _ComputeSalesAmount(saleDto, merchandise);
-        _profitFromDay += profit;
-        return profit;
-    }
+	public int SellMerchandise(CustomerSaleDto saleDto) {
+		Merchandise merchandise = _merchandiseService.GetHeldMerchandise();
+		_merchandiseService.SetHeldMerchandise(null);
+		int profit = _ComputeSalesAmount(saleDto, merchandise);
+		_profitFromDay += profit;
+		return profit;
+	}
 
-    public void ResetProfitFromDay() {
-        _profitFromDay = 0;
-    }
+	public void ResetProfitFromDay() {
+		_profitFromDay = 0;
+	}
 
-    public int GetProfitFromDay() {
-        return _profitFromDay;
-    }
+	public int GetProfitFromDay() {
+		return _profitFromDay;
+	}
 
-    public void AddProfitFromDayToPlayerMoney() {
-        _playerDataService.SetMoney(_playerDataService.GetMoney() + _profitFromDay);
-        ResetProfitFromDay();
-    }
+	public void AddProfitFromDayToPlayerMoney() {
+		_playerDataService.SetMoney(_playerDataService.GetMoney() + _profitFromDay);
+		ResetProfitFromDay();
+	}
 
-    private int _ComputeSalesAmount(CustomerSaleDto saleDto, Merchandise merchandise) {
-        float amount = merchandise.Tier * ValuePerTier;
-        float baseTip = saleDto.customerId switch {
-            CustomerId.RichMale => 1,
-            CustomerId.RichFemale => 1,
-            CustomerId.RegularMale => .5f,
-            CustomerId.RegularFemale => .5f,
-            CustomerId.PoorMale => .1f,
-            CustomerId.PoorFemale => .1f,
-            _ => 1
-        };
-        float tipModifier = saleDto.customerMood switch {
-            CustomerMood.Happy => 1,
-            CustomerMood.Neutral => .5f,
-            CustomerMood.Angry => 0,
-            _ => 0
-        };
+	private int _ComputeSalesAmount(CustomerSaleDto saleDto, Merchandise merchandise) {
+		float amount = merchandise.Tier * ValuePerTier;
+		float baseTip = saleDto.customerId switch {
+			CustomerId.RichMale => 1,
+			CustomerId.RichFemale => 1,
+			CustomerId.RegularMale => .5f,
+			CustomerId.RegularFemale => .5f,
+			CustomerId.PoorMale => .1f,
+			CustomerId.PoorFemale => .1f,
+			_ => 1
+		};
+		float tipModifier = saleDto.customerMood switch {
+			CustomerMood.Happy => 1,
+			CustomerMood.Neutral => .5f,
+			CustomerMood.Angry => 0,
+			_ => 0
+		};
 
-        float totalTipRatio = 1 + (baseTip * tipModifier);
+		float totalTipRatio = 1 + (baseTip * tipModifier);
 
-        if (merchandise.Color != saleDto.colorWanted) {
-            amount /= 2;
-        }
+		if (merchandise.Color != saleDto.colorWanted) {
+			amount /= 2;
+		}
 
-        if (merchandise.Type != saleDto.merchandiseTypeWanted) {
-            amount /= 2;
-        }
+		if (merchandise.Type != saleDto.merchandiseTypeWanted) {
+			amount /= 2;
+		}
 
-        amount *= totalTipRatio;
+		amount *= totalTipRatio;
 
-        return Math.Max(1, (int)amount);
-    }
+		return Math.Max(1, (int)amount);
+	}
 }
